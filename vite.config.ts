f1afd27c5481754cf57e73d62add8748be72f599
Miguel Moroni <miguelmcontato@gmail.com>
2026-07-10 11:28:40 -0300

    fix: vite config ignored

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,16 @@ import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+	plugins: [tailwindcss(), sveltekit()],
+	server: {
+		watch: {
+			ignored: [
+				'**/flatpak/.flatpak-builder/**',
+				'**/flatpak/build-dir/**',
+				'**/flatpak/repo/**',
+				'**/flatpak/staging/**',
+				'**/flatpak/*.flatpak'
+			]
+		}
+	}
 });
